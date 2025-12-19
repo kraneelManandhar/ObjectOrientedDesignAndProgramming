0e03562199d8e6a5282a5e299a021d3a7ba673f9
kraneelManandhar <mydarling3547@gmail.com>
2025-12-19 23:20:58 +0545

    Qn 11

--- week5/src/w5/Qn11.java
+++ week5/src/w5/Qn11.java
@@ -1,13 +1,40 @@
 package w5;
 
-class Library{
-	
-}
+class Library {
+	    Book[] books;
 
-class Book{
-	private Book[] bk;
-}
+	    Library(Book[] books) {
+	        this.books = books;
+	    }
+	}
+
+
+class Book {
+	    String title;
+
+	    Book(String title) {
+	        this.title = title;
+	    }
+
+	    void show() {
+	        System.out.println(title);
+	    }
+	}
+
 
 public class Qn11 {
+	 public static void main(String[] args) {
 
+	        Book b1 = new Book("Java");
+	        Book b2 = new Book("Python");
+
+	        Book[] bookList = { b1, b2 };
+
+	        Library lib = new Library(bookList);
+
+	        lib = null;
+
+	        b1.show();
+	        b2.show();
+	    }
 }
