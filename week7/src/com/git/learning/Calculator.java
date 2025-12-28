f371df156b92d1ba2430a51f6fa7f4698078e402
kraneelManandhar <mydarling3547@gmail.com>
2025-12-28 13:04:51 +0545

    Force track learning directory

---
+++ week7/src/com/git/learning/Calculator.java
@@ -0,0 +1,7 @@
+package com.git.learning;
+
+public class Calculator {
+	public static void main(String[] args) {
+		System.out.println("This is a Calculator.");
+	}
+}
---
+++ week7/src/com/git/learning/Utils.java
@@ -0,0 +1,7 @@
+package com.git.learning;
+
+public class Utils {
+	public static void main(String[] args) {
+		System.out.println("This is Utils.");
+	}
+}
